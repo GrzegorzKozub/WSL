227a1ddaead629f5ea295ed37411f8f0f167a68a
greg <grzegorz.kozub@gmail.com>
2019-08-19 20:54:36 +0200

    Setup git and openssh

--- bootstap.sh
+++ bootstap.sh
@@ -12,3 +12,13 @@ sed -i "s/#umask 022/umask 022/g" ~/.profile
 sudo apt update
 sudo apt upgrade --assume-yes
 
+# git and openssh
+
+sudo apt install --assume-yes git openssh-client
+
+cp ./rootfs/home/greg/.gitconfig ~
+mkdir ~/.ssh
+cp /mnt/c/Users/Greg/.ssh/config ~/.ssh
+cp -r /mnt/c/Users/Greg/.ssh/github.com ~/.ssh
+chmod 600 ~/.ssh/github.com/id_rsa
+
