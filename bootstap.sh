f85ae31f03c0951321489c2c6249ffeea2e5d225
greg <grzegorz.kozub@gmail.com>
2019-08-20 08:20:36 +0200

    Setup erlang and elixir

--- bootstap.sh
+++ bootstap.sh
@@ -136,3 +136,18 @@ done
 
 ~/go/bin/gometalinter --install
 
+# erlang and elixir
+
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+rm erlang-solutions_1.0_all.deb
+
+sudo apt update
+sudo apt install --assume-yes esl-erlang elixir
+
+mix local.hex --force
+mix local.rebar --force
+mix archive.install hex phx_new --force
+
+cp ./rootfs/home/greg/.iex.exs ~
+
