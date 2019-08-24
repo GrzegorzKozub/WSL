d458e1855caf40abd7b21f8ae8af3e931577ffbb
greg <grzegorz.kozub@gmail.com>
2019-08-24 16:53:10 +0200

    Fix git permissions bug

--- git.sh
+++ git.sh
@@ -6,5 +6,6 @@ cp ./rootfs/home/greg/.gitconfig ~
 mkdir ~/.ssh
 cp /mnt/c/Users/Greg/.ssh/config ~/.ssh
 cp -r /mnt/c/Users/Greg/.ssh/github.com ~/.ssh
+chmod 600 ~/.ssh/config
 chmod 600 ~/.ssh/github.com/id_rsa
 
