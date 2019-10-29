73b9028475117f9fc267711d4565a03125b2ce95
greg <grzegorz.kozub@gmail.com>
2019-10-29 08:41:48 +0100

    Remove yarn in favor of npm

--- nodejs.sh
+++ nodejs.sh
@@ -1,29 +1,20 @@
-# nodejs and yarn
+# nodejs
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
 sudo apt install --assume-yes nodejs
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt update
-sudo apt install --assume-yes --no-install-recommends yarn
-
-yarn global add \
+npm install --global \
   @angular/cli \
-  babel-cli \
   create-react-app \
   eslint \
   express-generator \
-  gulp-cli \
   js-beautify \
-  karma-cli \
   neovim \
-  pm2 \
   rimraf \
   tslint \
   typescript \
-  typescript-formatter \
-  yo
+  typescript-formatter
 
+cp ./rootfs/home/greg/.angular-config.json ~
+cp ./rootfs/home/greg/.npmrc ~
