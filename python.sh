022ad40ec8bbe8721087635a01db0324955072be
greg <grzegorz.kozub@gmail.com>
2019-11-28 17:56:03 +0100

    Install lastversion

--- python.sh
+++ python.sh
@@ -2,6 +2,6 @@
 
 sudo apt install --assume-yes python python-pip
 
-pip install awscli aws-shell pynvim --user
+pip install awscli aws-shell lastversion pynvim --user
 pip install vim-vint --user --pre
 
