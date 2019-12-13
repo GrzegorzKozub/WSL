937da1851663552bde34e0be8f298f71258a7ca1
greg <grzegorz.kozub@gmail.com>
2019-12-13 08:53:50 +0100

    Fix permissions so zsh stops complaining on startup

--- zsh.sh
+++ zsh.sh
@@ -5,6 +5,7 @@ sudo apt install --assume-yes zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+chmod a-w,u+w ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp -r ./rootfs/home/greg/.oh-my-zsh/custom/themes ~/.oh-my-zsh/custom
 cp ./rootfs/home/greg/.zshrc ~
